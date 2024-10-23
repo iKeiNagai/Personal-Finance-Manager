1e603fedc45fc539367381e99588554fd21702bf
iKeiNagai <thomasvasquez68@gmail.com>
2024-10-23 17:35:27 -0400

    added insert transaction button

--- finance_manager/lib/account.dart
+++ finance_manager/lib/account.dart
@@ -1,7 +1,8 @@
+import 'package:finance_manager/databasehelper.dart';
 import 'package:flutter/material.dart';
 
 class Account extends StatefulWidget {
-  final String account;
+  final Map<String, dynamic> account;
   Account({required this.account});
 
   @override
@@ -9,21 +10,76 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
+  TextEditingController _amounttController = TextEditingController();
+  TextEditingController _categoryController = TextEditingController();
+  TextEditingController _dateController = TextEditingController();
+  
+  bool _isPositive = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF282A36),
       appBar: AppBar(
-        backgroundColor: Color(0xFF44475A),
         title: Text('Account'),
       ),
       body: Center(
         child: Column(
+
           children: <Widget>[
-            Text('Account')
+            Text(widget.account['balance'].toString()),
+        
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){
+          showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: Text('Insert Transaction'),
+                  content: StatefulBuilder(
+                    builder: (BuildContext context, StateSetter setState){
+                      return Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            controller: _amounttController,
+                            decoration: InputDecoration(hintText:'Amount'),
+                          ),
+                          SizedBox(height: 20),
+                          TextField(
+                            controller: _categoryController,
+                            decoration: InputDecoration(hintText: 'Category'),
+                          ),
+                          SizedBox(height:20),
+                          TextField(
+                            controller: _dateController,
+                            decoration: InputDecoration(hintText: 'Date: YYYY-MM-DD'),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(_isPositive ? '-' : '+'),
+                              Switch(
+                                value: _isPositive, 
+                                onChanged: (value){
+                                  setState((){
+                                    _isPositive = value;
+                                  });
+                                })
+                            ],
+                          ),
+                          ElevatedButton(
+                            onPressed: (){
+
+                            }, 
+                            child: Text('Save'))
+                        ],
+                      );
+                    }),
+                ));
+        },
+        child: Icon(Icons.add)),
     );
   }
 }
